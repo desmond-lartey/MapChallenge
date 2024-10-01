19eb2359611848dd518e580da4c68495b13bd915
Desmond Lartey <larteydesmond3@gmail.com>
2024-10-01 20:37:56 +0200

    v

---
+++ 2024/Day16/index.js
@@ -0,0 +1,43 @@
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [125, 10]
+
+    },
+    "properties": {
+        "name": "Dignate Island",
+        "population": 124567
+    }
+}
+
+
+
+{
+    "type": "Feature",
+    "geometry": {
+        "type": "'Point",
+        "coordinates": [125, 10]
+    },
+    "properties": {
+        "name": "Dignate Island"
+        "populations": 12345
+    }
+}
+
+
+{
+    "type": "Faeture",
+    "geometry": {
+        "type": "Point",
+        "coordinates" : [125, 10]
+    },
+    "propoerties": {
+        "name": "Dignat Island"
+        "Population": 574938
+    }
+}
+
+
+let todayInterview = "fourpeople"
+
---
+++ 2024/Day16/index.ts
@@ -0,0 +1,4 @@
+
+let todayinterview : any = "threepeople"
+
+todayinterview = 4
